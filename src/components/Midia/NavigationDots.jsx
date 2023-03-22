6f25c3b51718889eed16273504215bbded5c2f05
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-22 14:49:37 -0300

    ate aqui ok

--- src/components/Midia/NavigationDots.jsx
+++ src/components/Midia/NavigationDots.jsx
@@ -10,7 +10,7 @@ const NavigationDots = ({ active }) => (
         href={`#${itens}`}
         key={itens + index}
         className="app__navigation-dot"
-        style={active === itens ? {} : {}}/>
+        style={active === itens ? { backgroundColor: "#313BAC" } : {}}/>
        
       
     ))}
--- src/container/home/Home.jsx
+++ src/container/home/Home.jsx
@@ -16,7 +16,7 @@ const scalevariantes={
 }
 function Home() {
   return (
-    <div id='home'  className="app__header app__flex">
+    <div id='home'  className="app app__flex">
       <motion.div
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
