9b23f16f6fcbc98191b5e602cf318f542389094f
Robervan Souza <103292038+RobervanSouza@users.noreply.github.com>
2023-03-25 08:17:17 -0300

    ok sobre

--- src/App.jsx
+++ src/App.jsx
@@ -24,36 +24,7 @@ const App = () => {
       </div>
 
       <div className="app-sobre">
-        <div className="bolas1">
-          <p style={{ "--i": 1 }}></p>
-          <p style={{ "--i": 11}}></p>
-          <p style={{ "--i": 21 }}></p>
-          <p style={{ "--i": 31 }}></p>
-          <p style={{ "--i": 2 }}></p>
-          <p style={{ "--i": 12 }}></p>
-          <p style={{ "--i": 22 }}></p>
-          <p style={{ "--i": 3 }}></p>
-          <p style={{ "--i": 13 }}></p>
-          <p style={{ "--i": 23 }}></p>
-          <p style={{ "--i":  4 }}></p>
-          <p style={{ "--i": 14 }}></p>
-          <p style={{ "--i": 24 }}></p>
-          <p style={{ "--i": 5  }}></p>
-          <p style={{ "--i": 15 }}></p>
-          <p style={{ "--i": 25 }}></p>
-          <p style={{ "--i": 6  }}></p>
-          <p style={{ "--i": 16 }}></p>
-          <p style={{ "--i": 26 }}></p>
-          <p style={{ "--i": 7  }}></p>
-          <p style={{ "--i": 17 }}></p>
-          <p style={{ "--i": 27}}></p>
-          <p style={{ "--i": 8 }}></p>
-          <p style={{ "--i": 18}}></p>
-          <p style={{ "--i": 28}}></p>
-          <p style={{ "--i": 9 }}></p>
-          <p style={{ "--i": 19}}></p>
-          <p style={{ "--i": 29}}></p>
-        </div>
+       
         <Sobre />
       </div>
 
--- src/container/sobre/Sobre.jsx
+++ src/container/sobre/Sobre.jsx
@@ -36,7 +36,36 @@ function Sobre() {
     <>
       <div id="sobre1" className="psobre">
         <div id="fundo"></div>
-
+        <div className="bolas1">
+          <p style={{ "--i": 1 }}></p>
+          <p style={{ "--i": 11 }}></p>
+          <p style={{ "--i": 21 }}></p>
+          <p style={{ "--i": 31 }}></p>
+          <p style={{ "--i": 2 }}></p>
+          <p style={{ "--i": 12 }}></p>
+          <p style={{ "--i": 22 }}></p>
+          <p style={{ "--i": 3 }}></p>
+          <p style={{ "--i": 13 }}></p>
+          <p style={{ "--i": 23 }}></p>
+          <p style={{ "--i": 4 }}></p>
+          <p style={{ "--i": 14 }}></p>
+          <p style={{ "--i": 24 }}></p>
+          <p style={{ "--i": 5 }}></p>
+          <p style={{ "--i": 15 }}></p>
+          <p style={{ "--i": 25 }}></p>
+          <p style={{ "--i": 6 }}></p>
+          <p style={{ "--i": 16 }}></p>
+          <p style={{ "--i": 26 }}></p>
+          <p style={{ "--i": 7 }}></p>
+          <p style={{ "--i": 17 }}></p>
+          <p style={{ "--i": 27 }}></p>
+          <p style={{ "--i": 8 }}></p>
+          <p style={{ "--i": 18 }}></p>
+          <p style={{ "--i": 28 }}></p>
+          <p style={{ "--i": 9 }}></p>
+          <p style={{ "--i": 19 }}></p>
+          <p style={{ "--i": 29 }}></p>
+        </div>
         <h2 className="head-text">
           <div className="simbolos">
             <h1>
@@ -88,9 +117,7 @@ function Sobre() {
             </motion.div>
           ))}
         </div>
-  
-        </div>
-      
+      </div>
     </>
   );
 }
